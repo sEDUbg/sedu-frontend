8cffa5c0954b2b3dd691f750546d1ff2364244c3
Kaloyan Doychinov <kokolia1070@gmail.com>
2022-06-02 16:17:23 +0300

    [ADD] Loading animation

--- src/pages/Materials.jsx
+++ src/pages/Materials.jsx
@@ -58,21 +58,13 @@ const OLDgetGrid = async (type) => {
     return gridData;
 }
 
-const info = {
-    title: 'Ню Йорк сити',
-    link: '/materials/uuid=[uuid]',
-    thumbnail: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-    type: "presentation",
-    authors: [{ name: "Kaloyan Doychinov", profileUrl: "https://sedubg.com/", imageUrl: "https://avatars.cloudflare.steamstatic.com/b4a5d7f1473151316410c1307822efd74ec5a87b_full.jpg/" },], // profileURL = https://sedubg.com/user/id=[id]
-}
-
 const Material = ({ info }) => {
     // console.log(info)
     return (
         <Link to={info.link || '#'} className="flex flex-col dark:black hover:border hover:bg-gray-100 dark:hover:bg-slate-900 hover:border-gray-200 dark:hover:border-slate-800 rounded-2xl cursor-pointer overflow-hidden box-border">
-            <img src={info.thumbnail} className="thumbnail aspect-video rounded-2xl" />
+            <img src={info.thumbnail === '#' ? '/seduthumb.png' : info.thumbnail} className="thumbnail aspect-video rounded-2xl" />
             <div className="flex items-center p-3 space-x-4">
-                <Link to=''><img src={info.authors[0].imageUrl} className="w-12 block aspect-square rounded-full" /></Link>
+                <Link to=''><img src={info.authors[0].imageUrl || '/img/default.png'} className="w-12 block aspect-square rounded-full" /></Link>
                 <div className="flex flex-col">
                     <h3 className="text-xl font-bold">{info.title}</h3>
                     {info.authors.map((author, index) => (<p key={index} className="text-sm text-gray-500">{author.name}</p>))}
@@ -87,21 +79,23 @@ const Materials = ({ groupBy }) => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        setLoading(true);
         getGrid(groupBy)
             .then(grid => {
                 setGrid(grid);
             })
             .then(() => setLoading(false));
-            //.then(setTimeout(function() { setLoading(false); }, 5000));
+            //.then(setTimeout(function() { setLoading(false); }, 1000000));
 
     }, [groupBy]);
 
     if(loading) {
         return (
-            <div className="flex flex-col items-center justify-center h-screen">
-                <div className="w-64">
-                    <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="w-full h-full" />
-                </div>
+            <div className="flex flex-col items-center justify-center m-auto h-[80vh]">
+                <svg role="status" className="w-36 h-36 mr-2 text-gray-200 animate-spin dark:text-[#0F172A] fill-gray-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"></path>
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"></path>
+                </svg>
             </div>
         );
     }
