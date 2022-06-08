8a16f149a29a42a0729071e5d01181536c2d63d5
School_Bozhinov <marogo142005@gmail.com>
2022-06-08 12:12:19 +0300

    Profile Picture UserSettings

--- src/pages/UserSettings.jsx
+++ src/pages/UserSettings.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify'
-
+import { FileDrop } from 'react-file-drop';
 import { app } from '../utils/Firebase/firebase';
 import { doc, getDoc, getFirestore, updateDoc } from "firebase/firestore";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const schooList = [
   "91 Немска езикова гимназия 'Професор Константин Гълъбов'",
@@ -28,22 +29,62 @@ const UserSettings = () => {
   const [lastName, setLastName] = useState("");
   const [grade, setGrade] = useState("");
   const [bio, setBio] = useState("");
+  const [image, setImage] = useState("");
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState("");
   const school = useRef("");
+  const fileInputRef = useRef(null);
+
+  const onDropFile = (event) => {
+    //console.log(event[0]);
+    setFile(event[0]);
+    setFileName(event[0].name);
+  }
+
+  const onFileInputChange = (event) => {
+    const { files } = event.target;
+    //console.log(files[0]);
+    setFile(files[0]);
+    setFileName(files[0].name);
+  }
+
+  const onTargetClick = () => {
+    fileInputRef.current.click()
+  }
+
+  useEffect(() => {
+    if (file != null)
+      setImage(URL.createObjectURL(file));
+  }, [file]);
 
   const { id } = useParams();
 
   const handleAction = (e) => {
     e.preventDefault();
-    if (!userDataChange) {
+    // console.log(file)
+    if (userDataChange) {
+      const storage = getStorage(app);
+      const db = getFirestore(app);
+      const storageRef = ref(storage, 'Profile/' + file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on('state_changed', (snapshot) => {
+
+      }, (err) => {
+        console.log(err);
+      }, () => {
+        const url = 'https://storage.googleapis.com/sedubg-2022.appspot.com/Profile/' + file.name;
+
+        setUserDataChange(false);
+        setButton("промени");
+        const docRef = doc(db, 'StripeCustomers', id);
+        updateDoc(docRef, { FirstName: firstName, LastName: lastName, school: school.current.value, class: grade, bio: bio, profileUrl: url });
+        sessionStorage.setItem('ImageUrl', url);
+      });
+
+
+    } else {
       setUserDataChange(true);
       setButton("запази");
-    } else {
-      setUserDataChange(false);
-      setButton("промени");
-      console.log({ firstName: firstName, lastName: lastName, school: school, grade: grade, bio: bio });
-      const firestore = getFirestore(app);
-      const docRef = doc(firestore, 'StripeCustomers', id);
-      updateDoc(docRef, { FirstName: firstName, LastName: lastName, school: school.current.value, class: grade, bio: bio });
     }
   }
 
@@ -79,15 +120,16 @@ const UserSettings = () => {
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-      // console.log(docSnap.data());
+      console.log(docSnap.data());
       // setUserData(docSnap.data());
       setFirstName(docSnap.data().FirstName)
       setLastName(docSnap.data().LastName)
       setEmail(docSnap.data().email)
       setUsername(docSnap.data().Username)
-      school.current.value = docSnap.data().school;
       setGrade(docSnap.data().class)
       setBio(docSnap.data().bio)
+      setImage(docSnap.data().profileUrl)
+      school.current.value = docSnap.data().school;
 
     } else {
       console.log('No such document!');
@@ -96,14 +138,21 @@ const UserSettings = () => {
   }
 
   useEffect(() => {
-    getData().then(() => {
-      console.log(userData)
-    })
+    getData()
   }, []);
+
 
   return (
     <div className='w-screen flex-col items-center justify-center bg-gray-bg1 dark:text-white p-5'>
-      <img src={userData?.profileUrl || '/img/default.png'} className='m-auto rounded-full border w-44 dark:border-slate-800 -mb-24' />
+      {!userDataChange ? <img src={image || '/img/default.png'} className='m-auto rounded-full border w-44 dark:border-slate-800 -mb-24' /> : <div> <FileDrop onTargetClick={onTargetClick} onDrop={onDropFile} >
+        <img src={image || '/img/default.png'} className='m-auto rounded-full border w-44 dark:border-slate-800 -mb-24' />
+      </FileDrop>
+        <input
+          onChange={onFileInputChange}
+          ref={fileInputRef}
+          type="file"
+          className="hidden"
+        /> </div>}
       <div className='w-full max-w-md m-auto bg-white dark:bg-slate-900 rounded-lg border dark:border-slate-700 shadow-default pt-28 py-10 sm:px-16 px-8 divide-y divide-slate-600 space-y-4'>
         <div className=''>
           <form onSubmit={handleAction}>
